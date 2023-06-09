737330fdc8e6ea9f35a4c02515cc239618b10908
jspl151 <jayasutha151@gmail.com>
2023-06-09 07:08:46 +0000

    Marksheet Date csv to JSON.

--- index.js
+++ index.js
@@ -1,69 +1,13 @@
-const data = [
-  {
-    student: 'Sriram',
-    rollNo: 11,
-    tamil: 80,
-    english: 90,
-    science: 86,
-    maths: 97,
-    social: 76,
-  },
-  {
-    student: 'Ram',
-    rollNo: 16,
-    tamil: 60,
-    english: 97,
-    science: 100,
-    maths: 34,
-    social: 100,
+const csvToJson = require('csvtojson');
 
-  },
-  {
-    student: 'sri',
-    rollNo: 18,
-    tamil: 92,
-    english: 96,
-    science: 99,
-    maths: 99,
-    social: 87,
-  },
-  {
-    student: 'mani',
-    rollNo: 20,
-    tamil: 43,
-    english: 70,
-    science: 90,
-    maths: 83,
-    social: 86,
-  },
-  {
-    student: 'muthu',
-    rollNo: 20,
-    tamil: 45,
-    english: 53,
-    science: 60,
-    maths: 38,
-    social: 51,
-  },
-  {
-    student: 'malar',
-    rollNo: 20,
-    tamil: 75,
-    english: 70,
-    science: 90,
-    maths: 78,
-    social: 59,
-  },
-];
-
-const getTotal = (markSheet) => markSheet.tamil + markSheet.english + markSheet.science + markSheet.maths + markSheet.social;
+const getTotal = (markSheet) => parseInt(markSheet.tamil) + parseInt(markSheet.english) + parseInt(markSheet.science) + parseInt(markSheet.maths) + parseInt(markSheet.social);
 
 const getResult = (markSheet) => Math.min(markSheet.tamil, markSheet.english, markSheet.science, markSheet.maths, markSheet.social) >= 35 ? 'pass' : 'fail';
 
 const addFields = (markSheet) => ({
-  ...markSheet , 
-  total : getTotal(markSheet),
-  result : getResult(markSheet)
+  ...markSheet,
+  total: getTotal(markSheet),
+  result: getResult(markSheet)
 });
 
 const sortByTotal = (markSheets) => {
@@ -73,30 +17,34 @@ const sortByTotal = (markSheets) => {
 }
 
 const addRank = (sortedList) => {
-  const rankedList = sortedList.map((student,index,array) => 
+  const rankedList = sortedList.map((student, index, array) =>
   ({
-  ...student ,
-  rank : (student.result === 'pass')
-  ? array.filter((value) => ((value.result === 'pass') && (value.total > student.total))).length+1
-  : '-'
+    ...student,
+    rank: (student.result === 'pass')
+      ? array.filter((value) => ((value.result === 'pass') && (value.total > student.total))).length + 1
+      : '-'
   }));
-  
-  return rankedList ;
+
+  return rankedList;
 };
 
-const getCount = (rankedList) => rankedList.reduce((acc,markSheet)=>
-    ({
-    ...acc, 
-    pass: markSheet.result === 'pass' ? acc.pass+1 : acc.pass,
-    fail : markSheet.result === 'fail' ? acc.fail+1 : acc.fail
-    }),{pass : 0 , fail:0});
-  
-const main = () => {
+const getCount = (rankedList) => rankedList.reduce((acc, markSheet) =>
+({
+  ...acc,
+  pass: markSheet.result === 'pass' ? acc.pass + 1 : acc.pass,
+  fail: markSheet.result === 'fail' ? acc.fail + 1 : acc.fail
+}), { pass: 0, fail: 0 });
+
+const main = async () => {
+  const data = await csvToJson().fromFile('./markSheetData.csv');
+
+  //const main = () => {
   const studentDataList = data.map(addFields);
   const sortedList = sortByTotal(studentDataList);
   const rankedList = addRank(sortedList);
-  console.table(rankedList);
+  console.table(addRank(sortedList));
   console.log(getCount(rankedList));
+
 };
 
 main();
